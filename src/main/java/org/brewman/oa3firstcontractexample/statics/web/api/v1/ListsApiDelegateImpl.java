b0c5f16827a8871448f02833a554b66aa397bf67
Daniel Shiplett <daniel.shiplett@g2-inc.com>
2020-07-30 15:14:27 -0400

    Moved OpenAPI file and renamed.  In preparation for multifile.

--- src/main/java/org/brewman/oa3firstcontractexample/statics/web/api/v1/ListsApiDelegateImpl.java
+++ src/main/java/org/brewman/oa3firstcontractexample/statics/web/api/v1/ListsApiDelegateImpl.java
@@ -1,16 +1,12 @@
 package org.brewman.oa3firstcontractexample.statics.web.api.v1;
 
-import com.google.common.collect.ImmutableList;
 import org.brewman.oa3firstcontractexample.statics.service.StaticsService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
 
 @Component
 public class ListsApiDelegateImpl implements ListsApiDelegate {
